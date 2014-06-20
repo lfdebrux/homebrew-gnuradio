afb2245a5981fefcd805ce61d77bf3bc5b8e4b10
Laurence de Bruxelles <lfdebrux@gmail.com>
2014-06-20 17:55:59 +0100

    update gr-osmosdr recipe
    due to shtirlic/homebrew-gnuradio

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -2,23 +2,19 @@ require 'formula'
 
 class GrOsmosdr < Formula
   homepage 'http://sdr.osmocom.org/trac/wiki/GrOsmoSDR'
-  head 'git://git.osmocom.org/gr-osmosdr'
+  url 'http://git.osmocom.org/gr-osmosdr', :using => :git, :tag => 'v0.1.1'
+  head 'http://git.osmocom.org/gr-osmosdr', :using => :git
 
   depends_on 'cmake' => :build
   depends_on 'gnuradio'
-  depends_on 'rtlsdr'
+  depends_on 'librtlsdr'
 
   def install
     mkdir 'build' do
-      system 'cmake', '..', *std_cmake_args << "-DPYTHON_LIBRARY=#{python_path}/Frameworks/Python.framework/"
+      system 'cmake', '..', *std_cmake_args
       system 'make'
       system 'make install'
     end
   end
 
-  def python_path
-    python = Formula.factory('python')
-    kegs = python.rack.children.reject { |p| p.basename.to_s == '.DS_Store' }
-    kegs.find { |p| Keg.new(p).linked? } || kegs.last
-  end
 end
